fc0d32507243d5ed95d9708ddecb95468d7050de
Natalia Gorobtsova <gorobzova_natalia@mail.ru>
2022-01-28 01:35:50 +0300

    cards added

--- scripts/index.js
+++ scripts/index.js
@@ -44,6 +44,77 @@ popupCloseButton.addEventListener('click', closeButtonClick);
 formElement.addEventListener('submit', formSubmitHandler); 
 
 
+// Проектная работа 5
+
+/*const cardAddButton = document.querySelector ('.profile__add-button');
+const popupAddCard = document.querySelector('.popup_tipe_card');
+const popupCardCloseButton = document.querySelector('.popup__close-button_type_card');
+const cardLikeButton = document.querySelector('.element__icon')
+
+function cardAddButtonClick() {
+  popupAddCard.classList.toggle ('popup_opened');
+};
+
+function cardCloseButtonClick() {
+  popupAddCard.classList.toggle('popup_opened')
+}
+
+function cardLikeButtonClick() {
+  cardLikeButton.classList.toggle('element__icon_state_active')
+}
+cardAddButton.addEventListener('click', cardAddButtonClick);
+popupCardCloseButton.addEventListener('click', cardCloseButtonClick);
+cardLikeButton.addEventListener('click', cardLikeButtonClick);*/
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+  
+]; 
+
+
+const cardTemplate = document.querySelector('.card-template').content;
+const sectionElements = document.querySelector('.elements');
+
+
+function render() {
+  initialCards.forEach(renderCards);
+}
+
+
+function renderCards (element) {
+  const cardElement = cardTemplate.cloneNode(true);
+  sectionElements.append(cardElement);
+  cardTemplate.querySelector('.element__name').textContent = element.name
+  cardTemplate.querySelector('.element__photo-img').src = element.link
+}
+
+render();
+
 
 
 
